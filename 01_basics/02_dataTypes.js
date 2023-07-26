f48c4ff733e6fa9f8c057ebaa4c39904d504f96a
kamalboroKB <53759359+kamalboroKB@users.noreply.github.com>
2023-07-26 06:44:51 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using nodejs, not browser
+
+let name = "Kamal" // string
+let age = 20 // number
+let isLoggedIn = true // boolian
+let state; // undefined value
+
+// number => 2 to the power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object =>
+
+console.log(typeof undefined); // data types 
